a00e897bd7ec8a702368f78d0005e903d96aa7c6
leo <leone.leo2010@gmail.com>
2026-09-01 20:37:57 +0200

    Add files via upload
    starting a project

---
+++ cat/cat.ino
@@ -0,0 +1,38 @@
+//cat ears
+//by leo.mandolini
+
+#include <ESP32Servo.h>
+
+leftear = 1;
+rightear = 0;
+
+Servo sleft;
+Servo sright;
+
+void setup() {
+
+  sleft.attach(leftear);
+  sright.attach(rightear);
+
+  //just do something
+
+  sleft.write(0);
+  sright.write(0);
+
+  delay(10000);
+  
+  sleft.write(180);
+  sright.write(180);
+
+  delay(1000);
+
+  sleft.write(0);
+  sright.write(0);
+
+  delay(1000);
+}
+
+void loop() {
+
+
+}
