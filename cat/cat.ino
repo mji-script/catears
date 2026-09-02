50cacd9687423e5e1e5828b078d6b1ad7aaf17b6
leo <leone.leo2010@gmail.com>
2026-09-02 11:31:04 +0200

    Implement sensor input and servo movement logic
    Added sensor reading and servo control functionality.

--- cat/cat.ino
+++ cat/cat.ino
@@ -9,6 +9,8 @@
 const int leftear = 1;
 const int rightear = 0;
 
+const int sensor = 3;
+
 //servos
 
 Servo sleft;
@@ -16,7 +18,24 @@ Servo sright;
 
 //variables
 
+int value = 0;
+int angle = 0;
+int preangle = 0;
+
+//const
+
+int const noise = 1;
+
+//servo
+void catwrite(int degree) {
+  int mirror= map(degree, 0, 180, 180, 0);
+  sleft.write(degree);
+  sright.write(mirror);
+  return;
+}
+
 void setup() {
+  Serial.begin(115200);
 
   sleft.attach(leftear);
   sright.attach(rightear);
@@ -41,5 +60,18 @@ void setup() {
 
 void loop() {
 
+  value = analogRead(sensor);
+  angle = map(value, 0, 4095, 0, 180);
+
+  Serial.println(value);
+  Serial.println(angle);
+
+  //noise
+  if (!(abs(angle - preangle) <= noise)){
+    catwrite(angle);
+    preangle = angle;
+  }
+
+  delay(10);
 
 }
